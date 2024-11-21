67aebcaf25e511080924388aa553e60e19c0a154
adinngo <gnahtong123@gmail.com>
2024-11-21 17:04:42 +0700

    make finish task interactive

--- 16-task-tracker/main.js
+++ 16-task-tracker/main.js
@@ -2,47 +2,48 @@ const todoList = [
   {
     todo: "New task is created and added to the list",
     status: {
-      checked: false,
+      finished: false,
     },
   },
   {
     todo: "Clicking the checkbox toggles the completeness",
     status: {
-      checked: false,
+      finished: false,
     },
   },
   {
     todo: "Delete button will delete the task from the list",
     status: {
-      checked: false,
+      finished: false,
     },
   },
   {
     todo: "Complete tasks show at the end with strikethrough",
     status: {
-      checked: true,
+      finished: false,
     },
   },
   {
     todo: "Marking in complete will put it back in pending list",
     status: {
-      checked: true,
+      finished: false,
     },
   },
 ];
 renderTodoList();
 function renderTodoList() {
   let todoListHtml = "";
- 
+
   todoList.forEach((item, index) => {
+    const isFinished = item.status.finished;
     todoListHtml += `
       <div class="todoList-item">
         <label for="todo-status-${index}" class="todo-content-container">
-          <input type="checkbox" id="todo-status-${index}" class="todo-status" />
-          <span class="todo-content">${item.todo}</span>
+          <input type="checkbox" id="todo-status-${index}" class="todo-status" ${isFinished ? "checked" : ""}/>
+          <span class="todo-content ${isFinished ? "finished" : ""}">${item.todo}</span>
         </label>
         <div class="todo-content-edited-container">
-          <input type="text" class="input-todo-edited">
+          <input type="text" class="todo-input-edited">
           <button class="save-btn"><i class='bx bxs-save' ></i></button>
         </div>
         <button class="edit-btn"><i class='bx bx-edit'></i></button>
@@ -59,8 +60,30 @@ function renderTodoList() {
       todo: todoInput.value,
       status: true,
     });
-    
+
     renderTodoList();
   });
-}
 
+  document.querySelectorAll(".todoList-item").forEach((todoItem, index) => {
+    const finishTask = todoItem.querySelector(".todo-status");
+    const editBtn = todoItem.querySelector(".edit-btn");
+    const removeBtn = todoItem.querySelector(".remove-btn");
+
+    finishTask.addEventListener("click", () => {
+      // Toggle the finished state
+      todoList[index].status.finished = !todoList[index].status.finished;
+      renderTodoList();
+    });
+
+
+    removeBtn.addEventListener("click", () => {
+      todoList.splice(index, 1); // remove from list
+      renderTodoList();
+    });
+
+    editBtn.addEventListener("click", () => {
+      todoItem.classList.add("is-editing-todo-content");
+    });
+
+  });
+}
