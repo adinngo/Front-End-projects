0d500fc8b53db6f077e3972d04e1098ce99af13d
adinngo <gnahtong123@gmail.com>
2024-11-21 22:51:49 +0700

    create update delete interactive

--- 16-task-tracker/main.js
+++ 16-task-tracker/main.js
@@ -33,12 +33,74 @@ const todoListItem = JSON.parse(localStorage.getItem("todoListItem")) || [
 const todoLists = [
   {
     todoListTitle: "Task Tracker",
-    content: todoListItem,
+    content: [
+      {
+        todo: "New task is created and added to the list",
+        status: {
+          finished: false,
+        },
+      },
+      {
+        todo: "Clicking the checkbox toggles the completeness",
+        status: {
+          finished: false,
+        },
+      },
+      {
+        todo: "Delete button will delete the task from the list",
+        status: {
+          finished: false,
+        },
+      },
+      {
+        todo: "Complete tasks show at the end with strikethrough",
+        status: {
+          finished: false,
+        },
+      },
+      {
+        todo: "Marking in complete will put it back in pending list",
+        status: {
+          finished: false,
+        },
+      },
+    ],
   },
 
   {
     todoListTitle: "Do Workout",
-    content: todoListItem,
+    content: [
+      {
+        todo: "New task is created and added to the list",
+        status: {
+          finished: false,
+        },
+      },
+      {
+        todo: "Clicking the checkbox toggles the completeness",
+        status: {
+          finished: false,
+        },
+      },
+      {
+        todo: "Delete button will delete the task from the list",
+        status: {
+          finished: false,
+        },
+      },
+      {
+        todo: "Complete tasks show at the end with strikethrough",
+        status: {
+          finished: false,
+        },
+      },
+      {
+        todo: "Marking in complete will put it back in pending list",
+        status: {
+          finished: false,
+        },
+      },
+    ],
   },
 ];
 
@@ -54,7 +116,7 @@ renderTodoLists();
 
 function renderTodoLists() {
   let todoListsHtml = "";
-  todoLists.forEach((todoList) => {
+  todoLists.forEach((todoList, index) => {
     todoListsHtml += `
     <div class="todoList-container">
           <!-- title of todoList -->
@@ -71,25 +133,68 @@ function renderTodoLists() {
 
            <!-- content of todoList -->
           <div class="todoList">
-            ${renderTodoList()}
+            ${renderTodoList(todoList.content, index)}
           </div>
     </div>
   `;
   });
 
   document.querySelector(".todoLists-container").innerHTML = todoListsHtml;
+
+  document.querySelectorAll(".todoList-container").forEach((item, index) => {
+    item.querySelector(".add-btn").addEventListener("click", () => {
+      const todoInput = item.querySelector(".todo-input").value;
+      todoLists[index].content.push({ todo: todoInput, status: false });
+      renderTodoLists();
+    });
+
+    item.querySelectorAll(".todoList-item").forEach((todoItem, subIndex) => {
+      const finishTask = todoItem.querySelector(".todo-status");
+      const editBtn = todoItem.querySelector(".edit-btn");
+      const removeBtn = todoItem.querySelector(".remove-btn");
+      const saveBtn = todoItem.querySelector(".save-btn");
+
+      finishTask.addEventListener("click", () => {
+        // Toggle the finished state
+        todoLists[index].content[subIndex].status.finished = !todoLists[index].content[subIndex].status.finished;
+        // saveToLocalStorage();
+        renderTodoLists();
+      });
+
+      editBtn.addEventListener("click", () => {
+        todoItem.classList.add("is-editing-todo-content");
+        todoItem.querySelector(".todo-input-edited").value = todoList[index].todo;
+      });
+
+      saveBtn.addEventListener("click", () => {
+        todoLists[index].content[subIndex].todo = todoItem.querySelector(".todo-input-edited").value;
+        todoItem.classList.remove("is-editing-todo-content");
+        // saveToLocalStorage();
+        renderTodoLists();
+      });
+
+      removeBtn.addEventListener("click", () => {
+        todoLists[index].content.splice(subIndex, 1); // remove from list
+        // saveToLocalStorage();
+        renderTodoLists();
+      });
+
+    });
+  });
 }
 
-document.querySelectorAll(".todoList-container").forEach((item) => {});
-function renderTodoList() {
+// render sub todoList
+function renderTodoList(content, index) {
   let todoListHtml = "";
 
-  todoListItem.forEach((item, index) => {
+  content.forEach((item, subIndex) => {
     const isFinished = item.status.finished;
     todoListHtml += `
       <div class="todoList-item">
-        <label for="todo-status-${index}" class="todo-content-container">
-          <input type="checkbox" id="todo-status-${index}" class="todo-status" ${isFinished ? "checked" : ""}/>
+        <label for="todo-status-${index}-${subIndex}" class="todo-content-container">
+          <input type="checkbox" id="todo-status-${index}-${subIndex}" class="todo-status" ${
+      isFinished ? "checked" : ""
+    }/>
           <span class="todo-content ${isFinished ? "finished" : ""}">${item.todo}</span>
         </label>
         <div class="todo-content-edited-container">
