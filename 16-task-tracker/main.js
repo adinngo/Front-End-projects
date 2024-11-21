ed3513d96e8770945502677c645830a9729b9944
adinngo <gnahtong123@gmail.com>
2024-11-21 15:43:27 +0700

    save data and generate todo list in html

--- 16-task-tracker/main.js
+++ 16-task-tracker/main.js
@@ -1,9 +1,51 @@
 const todoList = [
   {
-    id: 1,
     todo: "New task is created and added to the list",
     status: {
-      checked: false
-    }
-  }
+      checked: false,
+    },
+  },
+  {
+    todo: "Clicking the checkbox toggles the completeness",
+    status: {
+      checked: false,
+    },
+  },
+  {
+    todo: "Delete button will delete the task from the list",
+    status: {
+      checked: false,
+    },
+  },
+  {
+    todo: "Complete tasks show at the end with strikethrough",
+    status: {
+      checked: true,
+    },
+  },
+  {
+    todo: "Marking in complete will put it back in pending list",
+    status: {
+      checked: true,
+    },
+  },
 ];
+let todoListHtml = "";
+todoList.forEach((item, index) => {
+  todoListHtml += `
+    <div class="todoList-item">
+      <label for="todo-status-${index}" class="todo-content-container">
+        <input type="checkbox" id="todo-status-${index}" class="todo-status" />
+        <span class="todo-content">${item.todo}</span>
+      </label>
+      <div class="todo-content-edited-container">
+        <input type="text" class="input-todo-edited">
+        <button class="save-btn"><i class='bx bxs-save' ></i></button>
+      </div>
+      <button class="edit-btn"><i class='bx bx-edit'></i></button>
+      <button class="remove-btn"><i class="bx bx-trash"></i></button>
+    </div>
+  
+  `;
+});
+document.querySelector(".todoList").innerHTML = todoListHtml;
